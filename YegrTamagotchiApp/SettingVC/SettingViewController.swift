d79162859fa8ad97ac74a2533d6345f709b3513d
yeggrrr <yjkim981103@gmail.com>
2024-06-21 01:52:04 +0900

    [Feat] Cell 내용 추가

--- YegrTamagotchiApp/SettingVC/SettingViewController.swift
+++ YegrTamagotchiApp/SettingVC/SettingViewController.swift
@@ -8,6 +8,35 @@
 import UIKit
 import SnapKit
 
+enum Setting: Int, CaseIterable {
+    case myName
+    case changeTamagotchi
+    case reset
+    
+    var optionImage: String {
+        switch self {
+        case .myName:
+            return "pencil"
+        case .changeTamagotchi:
+            return "star.fill"
+        case .reset:
+            return "arrow.clockwise"
+            
+        }
+    }
+    
+    var option: String {
+        switch self {
+        case .myName:
+            return "내 이름 설정하기"
+        case .changeTamagotchi:
+            return "다마고치 변경하기"
+        case .reset:
+            return "데이터 초기화"
+        }
+    }
+}
+
 class SettingViewController: UIViewController {
     let settingTableView = UITableView()
     
@@ -48,15 +77,21 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return Setting.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
         cell.accessoryType = .disclosureIndicator
-        cell.imageView?.image = UIImage(systemName: "star")
-        cell.textLabel?.text = "ddd"
-        cell.detailTextLabel?.text = "sss"
+        cell.imageView?.image = UIImage(systemName: Setting.allCases[indexPath.row].optionImage)
+        cell.imageView?.tintColor = .darkGray
+        cell.textLabel?.text = Setting.allCases[indexPath.row].option
+        cell.textLabel?.font = .systemFont(ofSize: 13)
+        if indexPath.row != 0 {
+            cell.detailTextLabel?.isHidden = true
+        }
+        cell.detailTextLabel?.text = "푸바오" // 예시
+        cell.detailTextLabel?.font = .systemFont(ofSize: 13)
         cell.selectionStyle = .none
         return cell
     }
