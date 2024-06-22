022e354827ad84d8e76ea3707b346dcbd51e1ab8
yeggrrr <yjkim981103@gmail.com>
2024-06-23 00:54:16 +0900

    [Feat] 닉네임 변경 조건 추가

--- YegrTamagotchiApp/SettingVC/UserNameSettingViewController.swift
+++ YegrTamagotchiApp/SettingVC/UserNameSettingViewController.swift
@@ -11,6 +11,14 @@ import SnapKit
 class UserNameSettingViewController: UIViewController {
     let textField = UITextField()
     let lineView = UIView()
+    let messageLabel = UILabel()
+    var nickNameErrorMessage: NickNameErrorMessage = .empty
+    
+    enum NickNameErrorMessage: String {
+        case empty = ""
+        case wrongLength = "2글자 이상 6글자 미만으로 설정해주세요"
+        case noError = "사용 가능한 닉네임이에요"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +40,7 @@ class UserNameSettingViewController: UIViewController {
     func configureHierarchy() {
         view.addSubview(textField)
         view.addSubview(lineView)
+        view.addSubview(messageLabel)
     }
     
     func configureLayout() {
@@ -46,16 +55,46 @@ class UserNameSettingViewController: UIViewController {
             $0.horizontalEdges.equalTo(safeArea).inset(30)
             $0.height.equalTo(1)
         }
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom).offset(5)
+            $0.horizontalEdges.equalTo(safeArea).inset(30)
+            $0.height.equalTo(20)
+        }
     }
     
     func configureUI() {
+        textField.delegate = self
         textField.placeholder = "희망하는 이름을 입력해주세요"
         lineView.backgroundColor = .lightGray
+        messageLabel.textColor = .lightGray
+        messageLabel.textAlignment = .left
+        messageLabel.font = .systemFont(ofSize: 15)
+    }
+    
+    func nicknameCondition() {
+        guard let text = textField.text else { return }
+        if text.isEmpty {
+            nickNameErrorMessage = .empty
+        } else if text.count < 2 || text.count > 6 {
+            nickNameErrorMessage = .wrongLength
+        } else {
+            nickNameErrorMessage = .noError
+        }
+        
+        messageLabel.text = nickNameErrorMessage.rawValue
     }
     
     @objc func saveButtonClicked() {
+        guard nickNameErrorMessage == .noError else { return }
         guard let text = textField.text else { return }
         UserDefaults.standard.setValue(text, forKey: UserDefaultsInfo.nickname.rawValue)
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension UserNameSettingViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        nicknameCondition()
     }
 }
